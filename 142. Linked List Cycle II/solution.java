677fadfea5710a1d103e6e340afa2aa01ac47969
MorganIII <mosaab.morgan@gmail.com>
2023-03-16 08:13:37 +0200

    first commit

---
+++ 142. Linked List Cycle II/solution.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if(head == null || head.next == null) return null;
+        ListNode fast = head;
+        ListNode slow = head;
+        ListNode check = head;
+        while(fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if(slow == fast) {
+
+                while(check != fast) {
+                    check = check.next;
+                    fast = fast.next;
+                }
+                break;
+            }
+        }
+
+        if(fast == null || fast.next == null) {
+            return null;
+        }
+        return check;
+    }
+}
